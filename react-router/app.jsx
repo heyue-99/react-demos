987293e437322e471ac35ed613da358785241056
heyue <15071359861@163.com>
2017-04-13 20:11:31 +0800

    Update app.jsx

--- react-router/app.jsx
+++ react-router/app.jsx
@@ -1,8 +1,8 @@
-import App from 'test.jsx';
-import TodoList from 'todolist.jsx';
-import ListTodo from 'weiboform.jsx';
-import Home from 'home.jsx';
-import Music from 'music.jsx'
+import App from 'test.js';
+import TodoList from 'todolist.js';
+import ListTodo from 'weiboform.js';
+import Home from 'home.js';
+import Music from 'music.js'
 
 ReactDOM.render((
     <Router history={hashHistory}>
