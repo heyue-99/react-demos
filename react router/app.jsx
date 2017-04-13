9c4c9d5882cddadce766b01fd1940bbb7d464e5b
heyue-99 <1822740431@qq.com>
2017-04-13 19:40:07 +0800

    react-router

---
+++ react router/app.jsx
@@ -0,0 +1,16 @@
+import App from 'test.jsx';
+import TodoList from 'todolist.jsx';
+import ListTodo from 'weiboform.jsx';
+import Home from 'home.jsx';
+import Music from 'music.jsx'
+
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        	<IndexRoute component={Home} />
+        	<Route path="/weiboform" component={ListTodo} />
+       		<Route path="/todolist" component={TodoList} />
+       		<Route path="/music" component={Music} />
+       	</Route>
+    </Router>
+), document.getElementById('app'));
